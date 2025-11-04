c1b2a60801fa5ff14a2fab9e8f0a24e715f63eac
Bishnudatta Biswal <bubun579010@gmail.com>
2025-11-04 16:39:38 +0530

    Update Payload validator

--- CRUDApi/Validation/UpdatePayloadValidator.cs
+++ CRUDApi/Validation/UpdatePayloadValidator.cs
@@ -14,12 +14,12 @@ namespace CRUDApi.Validation
             // Name validation
             RuleFor(D => D.Name).NotEmpty().WithMessage("Name is required.")
                 .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.")
-                .Matches("^[A-Za-z]{2,50}$").WithMessage("Name must contain only letters (A–Z), without numbers or special characters.");
+                .Matches("^[A-Za-z][A-Za-z ]{1,49}$").WithMessage("Name must contain only letters (A–Z), without numbers or special characters.");
 
             // Description validation
             RuleFor(D => D.Description).NotEmpty().WithMessage("Description is required.")
                 .Length(10, 200).WithMessage("Description must be between 10 and 200 characters.")
-                .Matches("^[A-Za-z]{10,200}$").WithMessage("Description must contain only letters(A–Z) without numbers or special characters.");
+                .Matches("^[A-Za-z][A-Za-z .,!]{9,199}$").WithMessage("Description must contain only letters(A–Z) without numbers or special characters.");
         }
     }
 }
